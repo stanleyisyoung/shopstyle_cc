de7267646e1a4be734be2f2c952a6295846cc3fe
stanleyisyoung <youngsstanley@gmail.com>
2017-08-23 02:16:55 -0700

    clean up code

--- ebates.py
+++ ebates.py
@@ -32,23 +32,31 @@ def getPage(page_num, data):
     return data
 
 
-page_num = 1
-hd_true = 0
-hd_false = 0
-data = dict() # current page data
-
-data = getPage(page_num, data)
-
-while(data['more'] == True):
-    for x in range(0, 10): # optimize for any size page
-        if data['response'][x]['flags']['hd'] == True:
-            hd_true = hd_true + 1
-        elif data['response'][x]['flags']['hd'] == False:
-            hd_false = hd_false + 1
-
-        # print("Page: %d True: %d  False: %d" % (page_num, hd_true, hd_false))
-    page_num += 1
+def count_all(hd_true, hd_false):
+    page_num = 1
+    data = dict()
     data = getPage(page_num, data)
 
+    while(data['more'] == True):
+        for x in range(0, 10): # optimize for any size page
+            if data['response'][x]['flags']['hd'] == True:
+                hd_true = hd_true + 1
+            elif data['response'][x]['flags']['hd'] == False:
+                hd_false = hd_false + 1
 
-print("Total Page: %d True: %d  False: %d" % (page_num, hd_true, hd_false))
+        page_num += 1
+        data = getPage(page_num, data)
+
+    return (hd_true, hd_false)
+
+
+def main():
+    hd_true = 0
+    hd_false = 0
+
+    hd_true, hd_false = count_all(hd_true, hd_false)
+    print("hd-true: %d  hd-false: %d" % (hd_true, hd_false))
+
+
+if __name__ == '__main__':
+    main()
